273f06856dafe85f3ea693dfedd383d77d816152
shai bachar <shaibachar@gmail.com>
2018-08-05 15:13:02 +0300

    playing with nodejs

---
+++ nodejs/api/routes/orders.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+
+const Order = require("../models/order");
+const Product = require("../models/product");
+
+router.get("/", (req, res, next) => {
+  Order.find()
+    .select("product quantity _id")
+    .exec()
+    .then(docs => {
+      const response = {
+        count: docs.length,
+        orders: docs.map(doc => {
+          return {
+            orderId: doc._id,
+            product: doc.productId,
+            quantity: doc.quantity,
+            url: {
+              type: "GET",
+              description: "order",
+              url: "http://localhost:3000/orders/" + doc._id
+            }
+          };
+        })
+      };
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+router.post("/", (req, res, next) => {
+  Product.findById(req.body.productId)
+    .then(product => {
+      if (product) {
+        const order = new Order({
+          _id: mongoose.Types.ObjectId(),
+          quantity: req.body.quantity,
+          product: req.body.productId
+        });
+
+        return order.save();
+      }
+    })
+    .then(result => {
+      if (!result) {
+        res.status(404).json({
+          message: "Product not found"
+        });
+      } else {
+        console.log(result);
+        res.status(201).json(result);
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+router.get("/:orderId", (req, res, next) => {
+  const id = req.params.orderId;
+  Order.findById(id)
+    .select("_id product quantity")
+    .exec()
+    .then(result => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+router.patch("/:orderId", (req, res, next) => {
+  const id = req.params.productId;
+  res.status(200).json({
+    message: "update",
+    id: id
+  });
+});
+
+router.delete("/:orderId", (req, res, next) => {
+  const id = req.params.productId;
+  res.status(200).json({
+    message: "delete",
+    id: id
+  });
+});
+
+module.exports = router;
--- nodejs/app.js
+++ nodejs/app.js
@@ -1,10 +1,66 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
+
+// Configuration
+
+//DB connection start
+mongoose.connect(
+  "mongodb://localhost:27017/test",
+  { useNewUrlParser: true }
+);
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function callback() {
+  console.log("db connected");
+});
+
+exports.test = function(req, res) {
+  res.render("test");
+};
+// DB Connection end
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// CORS Handling
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin , X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status("200").json({});
+  }
+  next();
+});
+
+//Routes
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
 
 app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'it works'
-    });
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
 });
 
 module.exports = app;
