e16173719b79a0984a4932e397601bb326132df0
shai bachar <shaibachar@gmail.com>
2019-12-01 11:14:39 +0200

    hello

---
+++ ApachePoi/src/main/java/com/example/ApachePoi/ApachePoiApplication.java
@@ -0,0 +1,16 @@
+package com.example.ApachePoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cglib.core.internal.Function;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class ApachePoiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApachePoiApplication.class, args);
+    }
+
+}
